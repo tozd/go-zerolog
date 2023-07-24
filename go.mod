3c456a303a2dc8b0ab67111010cb155cb8d5f679
Mitar <mitar.git@tnode.com>
2023-07-24 13:05:33 +0200

    Initial version.

---
+++ go.mod
@@ -0,0 +1,37 @@
+module gitlab.com/tozd/go/zerolog
+
+go 1.20
+
+require (
+	github.com/alecthomas/kong v0.8.0
+	github.com/mattn/go-colorable v0.1.13
+	github.com/rs/zerolog v1.26.2-0.20220219153918-361cdf616a3c
+	gitlab.com/tozd/go/errors v0.5.0
+	gitlab.com/tozd/go/x v0.0.0-20220929111513-a49c8a610517
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.1-0.20211018174820-ff6d014e72d9 // indirect
+	github.com/imdario/mergo v0.3.12 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
+	github.com/whilp/git-urls v1.0.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/net v0.0.0-20210326060303-6b1517762897 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
+)
---
+++ zerolog.go
@@ -0,0 +1,464 @@
+package cli
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	stdlog "log"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/mattn/go-colorable"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gitlab.com/tozd/go/errors"
+	"gitlab.com/tozd/go/x"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	fileMode = 0o600
+)
+
+// Copied from zerolog/console.go.
+const (
+	colorRed = iota + 31
+	colorGreen
+	colorYellow
+	colorBlue
+
+	colorBold     = 1
+	colorDarkGray = 90
+)
+
+const (
+	DefaultConsoleType  = "color"
+	DefaultConsoleLevel = "info"
+	DefaulFileLevel     = "info"
+)
+
+//nolint:lll
+type Console struct {
+	Type  string        `placeholder:"TYPE" enum:"color,nocolor,json,disable" default:"${defaultLoggingConsoleType}" help:"Type of console logging. Possible: ${enum}. Default: ${defaultLoggingConsoleType}." yaml:"type" json:"type"`
+	Level zerolog.Level `short:"l" placeholder:"LEVEL" enum:"trace,debug,info,warn,error" default:"${defaultLoggingConsoleLevel}" help:"All logs with a level greater than or equal to this level will be written to the console. Possible: ${enum}. Default: ${defaultLoggingConsoleLevel}." yaml:"level" json:"level"`
+}
+
+func (c *Console) UnmarshalYAML(value *yaml.Node) error {
+	var tmp struct {
+		Type  string `yaml:"type" json:"type"`
+		Level string `yaml:"level" json:"level"`
+	}
+
+	// TODO: Limit only to known fields.
+	//       See: https://github.com/go-yaml/yaml/issues/460
+	err := value.Decode(&tmp)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	level, err := zerolog.ParseLevel(tmp.Level)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	c.Type = tmp.Type
+	c.Level = level
+
+	return nil
+}
+
+//nolint:lll
+type File struct {
+	Path  string        `placeholder:"PATH" type:"path" help:"Append logs to a file (as well)." yaml:"path" json:"path"`
+	Level zerolog.Level `placeholder:"LEVEL" enum:"trace,debug,info,warn,error" default:"${defaultLoggingFileLevel}" help:"All logs with a level greater than or equal to this level will be written to the file. Possible: ${enum}. Default: ${defaultLoggingFileLevel}." yaml:"level" json:"level"`
+}
+
+func (f *File) UnmarshalYAML(value *yaml.Node) error {
+	var tmp struct {
+		Path  string `yaml:"path" json:"path"`
+		Level string `yaml:"level" json:"level"`
+	}
+
+	// TODO: Limit only to known fields.
+	//       See: https://github.com/go-yaml/yaml/issues/460
+	err := value.Decode(&tmp)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	level, err := zerolog.ParseLevel(tmp.Level)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	f.Path = tmp.Path
+	f.Level = level
+
+	return nil
+}
+
+type LoggingConfig struct {
+	Log     zerolog.Logger `kong:"-" yaml:"-" json:"-"`
+	Logging struct {
+		Console Console `embed:"" prefix:"console." yaml:"console" json:"console"`
+		File    File    `embed:"" prefix:"file." yaml:"file" json:"file"`
+	} `embed:"" prefix:"logging." yaml:"logging" json:"logging"`
+}
+
+// filteredWriter writes only logs at Level or above.
+type filteredWriter struct {
+	Writer zerolog.LevelWriter
+	Level  zerolog.Level
+}
+
+func (w *filteredWriter) Write(p []byte) (n int, err error) {
+	return w.Writer.Write(p)
+}
+
+func (w *filteredWriter) WriteLevel(level zerolog.Level, p []byte) (n int, err error) {
+	if level >= w.Level {
+		return w.Writer.WriteLevel(level, p)
+	}
+	return len(p), nil
+}
+
+// Copied from zerolog/writer.go.
+type levelWriterAdapter struct {
+	io.Writer
+}
+
+func (lw levelWriterAdapter) WriteLevel(_ zerolog.Level, p []byte) (n int, err error) {
+	return lw.Write(p)
+}
+
+// Copied from zerolog/console.go.
+func colorize(s interface{}, c int, disabled bool) string {
+	if disabled {
+		return fmt.Sprintf("%s", s)
+	}
+	return fmt.Sprintf("\x1b[%dm%v\x1b[0m", c, s)
+}
+
+// formatError extracts just the error message from error's JSON.
+func formatError(noColor bool) zerolog.Formatter {
+	return func(i interface{}) string {
+		j, ok := i.([]byte)
+		if !ok {
+			return colorize("[error: value is not []byte]", colorRed, noColor)
+		}
+		var e struct {
+			Error string `json:"error,omitempty"`
+		}
+		err := json.Unmarshal(json.RawMessage(j), &e)
+		if err != nil {
+			return colorize(fmt.Sprintf("[error: %s]", err.Error()), colorRed, noColor)
+		}
+		return colorize(colorize(e.Error, colorRed, noColor), colorBold, noColor)
+	}
+}
+
+// Based on zerolog/console.go, but with different colors.
+func formatLevel(noColor bool) zerolog.Formatter {
+	return func(i interface{}) string {
+		var l string
+		if ll, ok := i.(string); ok {
+			switch ll {
+			case zerolog.LevelTraceValue:
+				l = colorize("TRC", colorBlue, noColor)
+			case zerolog.LevelDebugValue:
+				l = "DBG"
+			case zerolog.LevelInfoValue:
+				l = colorize("INF", colorGreen, noColor)
+			case zerolog.LevelWarnValue:
+				l = colorize("WRN", colorYellow, noColor)
+			case zerolog.LevelErrorValue:
+				l = colorize("ERR", colorRed, noColor)
+			case zerolog.LevelFatalValue:
+				l = colorize("FTL", colorRed, noColor)
+			case zerolog.LevelPanicValue:
+				l = colorize("PNC", colorRed, noColor)
+			default:
+				l = "???"
+			}
+		} else {
+			if i == nil {
+				l = "???"
+			} else {
+				l = strings.ToUpper(fmt.Sprintf("%s", i))[0:3]
+			}
+		}
+		return l
+	}
+}
+
+// Based on zerolog/console.go, but formatted to local timezone.
+// See: https://github.com/rs/zerolog/pull/415
+func formatTimestamp(timeFormat string, noColor bool) zerolog.Formatter {
+	if timeFormat == "" {
+		timeFormat = time.Kitchen
+	}
+	return func(i interface{}) string {
+		t := "<nil>"
+		switch tt := i.(type) {
+		case string:
+			ts, err := time.Parse(zerolog.TimeFieldFormat, tt)
+			if err != nil {
+				t = tt
+			} else {
+				t = ts.Local().Format(timeFormat)
+			}
+		case json.Number:
+			i, err := tt.Int64()
+			if err != nil {
+				t = tt.String()
+			} else {
+				var sec, nsec int64 = i, 0
+				switch zerolog.TimeFieldFormat {
+				case zerolog.TimeFormatUnixMs:
+					nsec = int64(time.Duration(i) * time.Millisecond)
+					sec = 0
+				case zerolog.TimeFormatUnixMicro:
+					nsec = int64(time.Duration(i) * time.Microsecond)
+					sec = 0
+				}
+				ts := time.Unix(sec, nsec)
+				t = ts.Format(timeFormat)
+			}
+		}
+		return colorize(t, colorDarkGray, noColor)
+	}
+}
+
+type eventError struct {
+	Error string `json:"error,omitempty"`
+	Stack []struct {
+		Name string `json:"name,omitempty"`
+		File string `json:"file,omitempty"`
+		Line int    `json:"line,omitempty"`
+	} `json:"stack,omitempty"`
+	Cause *eventError `json:"cause,omitempty"`
+}
+
+type eventWithError struct {
+	Error *eventError `json:"error,omitempty"`
+	Level string      `json:"level,omitempty"`
+}
+
+// consoleWriter writes stack traces for errors after the line with the log.
+type consoleWriter struct {
+	zerolog.ConsoleWriter
+	buf  *bytes.Buffer
+	out  io.Writer
+	lock sync.Mutex
+}
+
+func newConsoleWriter(noColor bool) *consoleWriter {
+	buf := &bytes.Buffer{}
+	w := zerolog.NewConsoleWriter()
+	// Embedded ConsoleWriter writes to a buffer, to which this consoleWriter
+	// appends a stack trace and only then writes it to stdout.
+	w.Out = buf
+	w.NoColor = noColor
+	w.TimeFormat = "15:04"
+	w.FormatErrFieldValue = formatError(w.NoColor)
+	w.FormatLevel = formatLevel(w.NoColor)
+	w.FormatTimestamp = formatTimestamp(w.TimeFormat, w.NoColor)
+
+	return &consoleWriter{
+		ConsoleWriter: w,
+		buf:           buf,
+		out:           colorable.NewColorable(os.Stdout),
+	}
+}
+
+func makeMessageBold(p []byte) ([]byte, errors.E) {
+	var event map[string]interface{}
+	d := json.NewDecoder(bytes.NewReader(p))
+	d.UseNumber()
+	err := d.Decode(&event)
+	if err != nil {
+		return p, errors.Errorf("cannot decode event: %w", err)
+	}
+
+	if event[zerolog.MessageFieldName] == "" || event[zerolog.MessageFieldName] == nil {
+		return p, nil
+	}
+
+	switch event[zerolog.LevelFieldName] {
+	case zerolog.LevelInfoValue, zerolog.LevelWarnValue, zerolog.LevelErrorValue, zerolog.LevelFatalValue, zerolog.LevelPanicValue:
+		// Passthrough.
+	default:
+		return p, nil
+	}
+
+	event[zerolog.MessageFieldName] = colorize(fmt.Sprintf("%s", event[zerolog.MessageFieldName]), colorBold, false)
+	return x.MarshalWithoutEscapeHTML(event)
+}
+
+func (w *consoleWriter) Write(p []byte) (int, error) {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	defer w.buf.Reset()
+
+	// Remember the length before we maybe modify p.
+	n := len(p)
+
+	var errE errors.E
+	if !w.NoColor {
+		p, errE = makeMessageBold(p)
+		if errE != nil {
+			return 0, errE
+		}
+	}
+
+	_, err := w.ConsoleWriter.Write(p)
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+
+	var event eventWithError
+	err = json.Unmarshal(p, &event)
+	if err != nil {
+		return 0, errors.Errorf("cannot decode event: %w", err)
+	}
+
+	level, _ := zerolog.ParseLevel(event.Level)
+
+	// Print a stack trace only on error or above levels.
+	if level < zerolog.ErrorLevel {
+		_, err = w.buf.WriteTo(w.out)
+		return n, errors.WithStack(err)
+	}
+
+	ee := event.Error
+	first := true
+	for ee != nil {
+		if !first {
+			w.buf.WriteString(colorize("\nthe above error was caused by the following error:\n\n", colorRed, w.NoColor))
+			if ee.Error != "" {
+				w.buf.WriteString(colorize(colorize(ee.Error, colorRed, w.NoColor), colorBold, w.NoColor))
+				w.buf.WriteString("\n")
+			}
+		}
+		first = false
+		if len(ee.Stack) > 0 {
+			w.buf.WriteString(colorize("stack trace (most recent call first):\n", colorRed, w.NoColor))
+			for _, s := range ee.Stack {
+				w.buf.WriteString(colorize(s.Name, colorRed, w.NoColor))
+				w.buf.WriteString("\n\t")
+				w.buf.WriteString(colorize(s.File, colorRed, w.NoColor))
+				w.buf.WriteString(colorize(":", colorRed, w.NoColor))
+				w.buf.WriteString(colorize(strconv.Itoa(s.Line), colorRed, w.NoColor))
+				w.buf.WriteString("\n")
+			}
+		}
+		ee = ee.Cause
+	}
+
+	_, err = w.buf.WriteTo(w.out)
+	return n, errors.WithStack(err)
+}
+
+func extractLoggingConfig(config interface{}) (*LoggingConfig, errors.E) {
+	configType := reflect.TypeOf(LoggingConfig{})
+	val := reflect.ValueOf(config).Elem()
+	typ := val.Type()
+	fields := reflect.VisibleFields(typ)
+	for _, field := range fields {
+		if field.Type == configType {
+			return val.FieldByIndex(field.Index).Addr().Interface().(*LoggingConfig), nil
+		}
+	}
+
+	return nil, errors.Errorf("logging config not found in struct %T", config)
+}
+
+func New(config interface{}) (*os.File, errors.E) {
+	loggingConfig, errE := extractLoggingConfig(config)
+	if errE != nil {
+		return nil, errors.Errorf("cannot extract logging config: %w", errE)
+	}
+
+	level := zerolog.Disabled
+	writers := []io.Writer{}
+	var file *os.File
+	switch loggingConfig.Logging.Console.Type {
+	case "color", "nocolor":
+		w := newConsoleWriter(loggingConfig.Logging.Console.Type == "nocolor")
+		writers = append(writers, &filteredWriter{
+			Writer: levelWriterAdapter{w},
+			Level:  loggingConfig.Logging.Console.Level,
+		})
+		if loggingConfig.Logging.Console.Level < level {
+			level = loggingConfig.Logging.Console.Level
+		}
+	case "json":
+		w := os.Stdout
+		writers = append(writers, &filteredWriter{
+			Writer: levelWriterAdapter{w},
+			Level:  loggingConfig.Logging.Console.Level,
+		})
+		if loggingConfig.Logging.Console.Level < level {
+			level = loggingConfig.Logging.Console.Level
+		}
+	}
+	if loggingConfig.Logging.File.Path != "" {
+		w, err := os.OpenFile(loggingConfig.Logging.File.Path, os.O_APPEND|os.O_WRONLY|os.O_CREATE, fileMode) //nolint:govet
+		if err != nil {
+			return nil, errors.Errorf("cannot open logging file: %w", err)
+		}
+		file = w
+		writers = append(writers, &filteredWriter{
+			Writer: levelWriterAdapter{w},
+			Level:  loggingConfig.Logging.File.Level,
+		})
+		if loggingConfig.Logging.Console.Level < level {
+			level = loggingConfig.Logging.File.Level
+		}
+	}
+
+	writer := zerolog.MultiLevelWriter(writers...)
+	logger := zerolog.New(writer).Level(level).With().Timestamp().Logger()
+
+	zerolog.SetGlobalLevel(zerolog.TraceLevel)
+	zerolog.TimestampFunc = func() time.Time {
+		return time.Now().UTC()
+	}
+	zerolog.TimeFieldFormat = "2006-01-02T15:04:05.000Z07:00"
+	zerolog.ErrorMarshalFunc = func(ee error) interface{} {
+		if ee == nil {
+			return json.RawMessage("null")
+		}
+
+		var j []byte
+		var err error
+		switch e := ee.(type) { //nolint:errorlint
+		case interface {
+			MarshalJSON() ([]byte, error)
+		}:
+			j, err = e.MarshalJSON()
+		default:
+			j, err = x.MarshalWithoutEscapeHTML(struct {
+				Error string `json:"error,omitempty"`
+			}{
+				Error: ee.Error(),
+			})
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "marshaling error \"%s\" into JSON during logging failed: %s\n", ee.Error(), err.Error())
+		}
+		return json.RawMessage(j)
+	}
+	zerolog.InterfaceMarshalFunc = func(v interface{}) ([]byte, error) {
+		return x.MarshalWithoutEscapeHTML(v)
+	}
+	log.Logger = logger
+	loggingConfig.Log = logger
+	stdlog.SetFlags(0)
+	stdlog.SetOutput(logger)
+
+	return file, nil
+}
